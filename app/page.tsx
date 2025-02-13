ae79bfc0b5a6688e4a9653eef1791fce2f5df2b9
mwanavina <mmwanavina@gmail.com>
2025-02-13 16:15:41 +0200

    Update page and layout titles and descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-100 to-cyan-100">
-      <h1 className="text-4xl font-bold mb-8 text-gray-800">Robot Simulations</h1>
-      <div className="space-y-4">
+      <h1 className="text-4xl font-bold mb-8 text-gray-800">
+      Vacuum Cleaner Robot Implementation & Simulations
+      </h1>
+      <div className="flex flex-col space-y-4"> 
         <Link href="/task1">
-          <Button className="w-64">Vacuum Cleaner Simulation</Button>
+          <Button className="w-64">TASK1 [easy,medium,hard]</Button>
         </Link>
         <Link href="/task2">
-          <Button className="w-64">MUBAS Corridor Robot Simulation</Button>
+          <Button className="w-64">TASK2 [easy,medium]</Button>
         </Link>
         <Link href="/task2.1">
-          <Button className="w-64">MUBAS Multi-Floor Robot Simulation</Button>
+          <Button className="w-64">TASK2[hard] Multi-Floor</Button>
         </Link>
       </div>
     </div>
